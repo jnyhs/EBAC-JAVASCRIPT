01a554877d0a86b683f7600162b9e4f9d07bc0f2
jnyhs <jennyhaner@gmail.com>
2022-08-27 09:20:50 -0300

    Exercícios JS

--- Exercícios/js/script.js
+++ Exercícios/js/script.js
@@ -12,8 +12,14 @@
         n3= prompt ("Informe a terceira nota: ") ; 
         n3= eval (n3) ;
 
-        media = (n1 + n2 + n3)/3 ; 
+        media = (n1 + n2 + n3)/3 ;
+
+        if(n1 > 0 && n1 < 10 && n2 > 0 && n2 < 10 && n3 > 0 && n3 < 10 ){
         alert(media);
+        }else{
+        alert (" Por favor, insira notas válidas de 0 a 10")
+        }
+
 
                
         }
